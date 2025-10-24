7731a86e1b61ce6e570f187ba7e3dec0358ae5ea
Stéphane Kuma <kumastephane@gmail.com>
2025-10-24 03:04:52 +0000

    Update: Upgrade dependencies to latest compatible versions
    - Updated 86 dependencies to latest compatible versions
    - Fixed test configuration to work with dependency injection
    - All tests passing
    - No breaking changes detected

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import package_info_plus
 import path_provider_foundation
 import share_plus
-import sqflite
+import sqflite_darwin
 import video_player_avfoundation
 import wakelock_plus
 
